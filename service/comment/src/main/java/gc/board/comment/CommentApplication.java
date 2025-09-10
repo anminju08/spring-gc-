18754ae93b79013f57996a4f8074ca9a3c8f836e
anminju08 <anminju0310@gmail.com>
2025-09-10 23:57:21 +0900

    first commit

---
+++ service/comment/src/main/java/gc/board/comment/CommentApplication.java
@@ -0,0 +1,12 @@
+package gc.board.comment;
+
+import gc.board.comment.CommentApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommentApplication.class, args);
+    }
+}
